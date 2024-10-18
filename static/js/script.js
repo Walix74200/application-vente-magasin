feb943fd13f621f04c05ff68fc08782f82e1ba66
Lucas <lucas.bonsergent@epita.fr>
2024-10-18 17:48:00 +0200

    Supression des fichiers pour les remplacer par une nouvelle version du projet

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function calculerPrixTotal() {
-    var quantite = document.getElementById('quantite').value;
-    var prixUnitaire = document.getElementById('prix_unitaire').value;
-    var prixTotal = quantite * prixUnitaire;
-    document.getElementById('prix_total').innerText = prixTotal.toFixed(2) + ' €';
-}
-
-// Exemple de script pour vérifier que le fichier JS est bien chargé
-console.log('JavaScript chargé avec succès');
